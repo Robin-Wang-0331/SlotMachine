27a7bb2e9236c87929b23452c59758b14388006f
Robin <wyjjkk1234@yahoo.com>
2015-02-14 14:08:28 -0500

    Cleaned game.ts

--- SlotMachine_YijiongWang200270704/Scripts/game.ts
+++ SlotMachine_YijiongWang200270704/Scripts/game.ts
@@ -2,8 +2,9 @@
 var stage: createjs.Stage;
 
 // Game Objects 
-var helloText: createjs.Text;
-var button: createjs.Bitmap;
+var game: createjs.Container;
+var background: createjs.Bitmap;
+
 
 function init() {
     canvas = document.getElementById("canvas");
@@ -16,26 +17,21 @@ function init() {
 }
 
 function gameLoop() {
-    helloText.rotation += 5;
-
 
     stage.update(); // Refreshes our stage
 }
 
 // Event handlers
 function buttonClicked() {
-    helloText.text = "Good Bye";
-    helloText.regX = helloText.getBounds().width * 0.5;
-    helloText.regY = helloText.getBounds().height * 0.5;
-    
+
 }
 
 function buttonOut() {
-    button.alpha = 1.0;
+
 }
 
 function buttonOver() {
-    button.alpha = 0.5;
+
 }
 
 
@@ -44,26 +40,8 @@ function buttonOver() {
 
 // Our Game Kicks off in here
 function main() {
-    // Bitmap Button
-    button = new createjs.Bitmap("assets/images/blueButton.png");
-    stage.addChild(button);
-    button.x = stage.canvas.width * 0.5;
-    button.y = stage.canvas.height * 0.5;
-    button.regX = 110;
-    button.regY = 110;
-
-    button.addEventListener("click", buttonClicked);
-    button.addEventListener("mouseover", buttonOver);
-    button.addEventListener("mouseout", buttonOut);
-   
-
-    // Label
-    helloText = new createjs.Text("Hello World!", "40px Consolas", "#000000");
-    stage.addChild(helloText);
-    helloText.x = stage.canvas.width * 0.5;
-    helloText.y = stage.canvas.height * 0.5;
-    helloText.regX = helloText.getBounds().width * 0.5;
-    helloText.regY = helloText.getBounds().height * 0.5;
+    // instantiate my game container
+    game = new createjs.Container();
 
     
 }
